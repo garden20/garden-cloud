62aca1e9ad91e920250d600ceb490f05fde8a3dd
Ryan Ramage <ryan.ramage@gmail.com>
2012-09-11 16:41:36 -0600

    Decorate the topbar with a btn to a users garden.

---
+++ static/js/topbar_decorator.js
@@ -0,0 +1,34 @@
+$(function(){
+    if ($('#dashboard-topbar').data('ready')) {
+        decorate();
+    } else {
+        $('#dashboard-topbar').on('ready', decorate);
+    }
+
+    function decorate() {
+        var userCtx = JSON.parse(decodeURI($('#dashboard-topbar-session').data('userctx')));
+        if (userCtx.name) {
+            $.getJSON('/_users/org.couchdb.user:' + userCtx.name , function(data){
+                if (data.space) {
+                    var link = 'http://' + data.space + '.' + window.location.host;
+
+                    var m_right = $('#dashboard-topbar-session').width() + 20;
+
+                    var $btn = $('<a class="btn" href="'+ link +'">Go to your garden</a>');
+                    $btn.css({
+                        position: 'absolute',
+                        top: '3px',
+                        right: '0',
+                        'margin-right': m_right + 'px',
+                        padding: '1px 4px',
+                        'font-size': '11px',
+                        'line-height': '15px'
+                    });
+                    $('#dashboard-topbar').append($btn);
+                }
+            })
+        }
+    }
+
+
+})
